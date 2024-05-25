723e980ba8995e76677f1edf6d7f3b6afe43a604
Hom Dahal <homdahal997@gmail.com>
2024-05-25 14:14:24 -0400

    posts route added and logic implemented to get entire posts database

---
+++ routes/posts.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+//Importing the data from our fake database file
+const posts = require('../data/posts.js');
+
+//////////////////
+// BASE PATH
+// - /api/posts
+//////////////////
+
+// Creating a GET route for the entire posts database.
+// This would be impractical in larger data sets.
+router.get('/', (req, res) => {
+    const links = [
+        {
+            href: 'posts/:id',
+            rel: ':id',
+            type: 'GET',
+        },
+    ];
+
+    res.json({ posts, links });
+});
